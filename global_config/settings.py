5c27d96d92572ad8765289929ee53d7fc7dd252f
puinenveturi <daniel.dudola@gmail.com>
2021-05-31 11:19:55 +0200

    disables admin page

--- global_config/settings.py
+++ global_config/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'bootstrap4',
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
