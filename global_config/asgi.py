f7f35e1961dbf7fddd969ae6b4507e2c4afead8a
puinenveturi <daniel.dudola@gmail.com>
2021-05-31 11:14:48 +0200

    renames global config directory

--- global_config/asgi.py
+++ global_config/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for project_dreadfort project.
+ASGI config for global_config project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_dreadfort.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'global_config.settings')
 
 application = get_asgi_application()
--- global_config/wsgi.py
+++ global_config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project_dreadfort project.
+WSGI config for global_config project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_dreadfort.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'global_config.settings')
 
 application = get_wsgi_application()
