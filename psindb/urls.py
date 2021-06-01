fe0a7c8efba9d8cf589ec0bb71431e5409f08b20
puinenveturi <daniel.dudola@gmail.com>
2021-06-02 00:41:35 +0200

    adds initial interaction viewer version

--- psindb/urls.py
+++ psindb/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('home', views.index, name='home'),
     path('entry/<str:uniprot_id>', views.entry, name='entry'),
+
+    path('network/<str:accession>', views.network, name='network')
     # path('protein/<str:protein_id>', views.protein, name='protein')
 ]
