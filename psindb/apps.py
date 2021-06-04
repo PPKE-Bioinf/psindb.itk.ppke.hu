5a38c8f872e0b6d5f7a18457a6af9921082127c4
Daniel Dudola <daniel.dudola@gmail.com>
2021-06-04 13:36:38 +0200

    swaps mysql lib for mariadb

--- psindb/apps.py
+++ psindb/apps.py
@@ -1,6 +1,20 @@
 from django.apps import AppConfig
+from psindb.util.db import DB
 
 
 class PsindbConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'psindb'
+    db_args = {
+        # "host": "psindb-db",
+        "host": "localhost",
+        "user": "daniel",
+        "passwd": "password",
+        "database": "PSDInteractome_v0_10",
+    }
+
+    def ready(self):
+        print("READY")
+        DB.db_args = self.db_args
+        DB.retries = 5
+        DB.connect()
---
+++ psindb/util/db.py
@@ -0,0 +1,61 @@
+import time
+import mariadb
+from contextlib import closing
+
+
+class DB:
+    conn = None
+    db_args = None
+    retries = None
+    retry_wait = 8
+    sql_client = None
+
+    # do not instantiate this class
+    def __new__(cls):
+        raise NotImplementedError
+
+    @classmethod
+    def connect(cls, sql_client=mariadb):
+        cls.sql_client = sql_client
+        cls.conn = sql_client.connect(**cls.db_args)
+        cls.conn.ping()
+
+    @classmethod
+    def execute_sql(cls, sql_string, data=None):
+        with closing(cls.conn.cursor()) as cur:
+            for i in range(cls.retries):
+                try:
+                    if data:
+                        query = cur.execute(sql_string, data)
+                    else:
+                        query = cur.execute(sql_string)
+
+                    # store the cursors content in a list
+                    results = list(cur.fetchall())
+
+                    # commit all transactions, better safe than sorry :)
+                    cls.conn.commit()
+
+                    return query, results
+
+                except (
+                        AttributeError, cls.sql_client.OperationalError
+                ) as e:
+                    print(
+                        f"DB connection lost. Reconnecting ({i})...")
+
+                    time.sleep(cls.retry_wait)
+
+                    try:
+                        db = cls.sql_client.connect()
+                        db.ping(True)
+                    except (
+                        AttributeError, cls.sql_client.OperationalError
+                    ) as e:
+                        print(
+                            "Can't establish DB connection, waiting for retry"
+                        )
+
+                        time.sleep(30)
+
+            raise Exception("Failed to establish DB connection")
