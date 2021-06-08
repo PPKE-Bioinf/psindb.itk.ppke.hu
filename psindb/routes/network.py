ce4eb4ee21843b26146896c6a0d6786bea55b6c6
Daniel Dudola <daniel.dudola@gmail.com>
2021-06-08 23:12:33 +0200

    adds network json integration

--- psindb/routes/network.py
+++ psindb/routes/network.py
@@ -2,11 +2,9 @@ from django.http import JsonResponse
 import json
 
 def network(request, accession):
-    print(f"ACCESSION requested: {accession}")
-
-    # read file
-    with open('psindb/routes/test.json', 'r') as myfile:
+    with open(
+            f'psindb/routes/network_json/{accession}.json', 'r'
+    ) as myfile:
         data = myfile.read()
 
-    real_test_json = json.loads(data)
-    return JsonResponse(real_test_json, safe=False)
+    return JsonResponse(json.loads(data), safe=False)
