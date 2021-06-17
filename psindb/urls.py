6f0086a6327be40d84a857befb21b63dc1cd99a4
Daniel Dudola <daniel.dudola@gmail.com>
2021-06-18 00:32:46 +0200

    adds description and initial manual page

--- psindb/urls.py
+++ psindb/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.index, name='routes'),
     path('entry/<str:uniprot_id>', views.entry, name='entry'),
     path('network/<str:accession>', views.network, name='network'),
     path('interactions', views.interactions, name='interactions'),
+    path('manual', TemplateView.as_view(template_name="manual.html")),
 ]
