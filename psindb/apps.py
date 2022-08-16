af786ca82da8421037eec7e69b10ba7de49d3cd4
Daniel Dudola <daniel.dudola@broadcom.com>
2022-08-16 18:55:15 +0200

    migration to DB server

--- psindb/apps.py
+++ psindb/apps.py
@@ -6,7 +6,7 @@ class PsindbConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'psindb'
     db_args = {
-        "host": "psindb-db",
+        "host": "10.3.19.179",
         "user": "daniel",
         "passwd": "password",
         "database": "PSDInteractome_v1_01",
